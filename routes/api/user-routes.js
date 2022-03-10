9b50e1b17f7979a5f95f57ae7bc179693339dbc7
YEET <joshuamani@hotmail.com>
2022-03-09 23:11:33 -0500

    validating login

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -50,6 +50,31 @@ router.post('/', (req, res) => {
     });
 });
 
+router.post('/login', (req, res) => {
+    // expects {email: 'lernantino@gmail.com', password: 'password1234'}
+  User.findOne({
+    where: {
+      email: req.body.email
+    }
+  }).then(dbUserData => {
+      const validPassword = dbUserData.checkPassword(req.body.password);
+      if (!validPassword) {
+          res.status(400).json({ message: 'Incorrect password!' });
+          return;
+      }
+
+      res.json({ user: dbUserData, message: 'Your are now logged in!' });
+      
+    if (!dbUserData) {
+      res.status(400).json({ message: 'No user with that email address!' });
+      return;
+    }
+
+    // res.json({ user: dbUserData });
+    
+    });
+});
+
 // PUT /api/users/1
 router.put('/:id', (req, res) => {
     // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
